c8dace6e01ce864314e5a97eb0e506f450b8026a
pmetodiev <plame478@gmail.com>
2023-01-29 11:38:35 +0200

    Initial commit

---
+++ src/test/java/BackEnd.java
@@ -0,0 +1,74 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class BackEnd {
+
+
+    private static WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Webdrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://shop.pragmatic.bg/admin/");
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(600, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void userNameAndPasswordTest() {
+        WebElement userName = driver.findElement(By.id("input-username"));
+        userName.sendKeys("admin");
+
+        WebElement password = driver.findElement(By.id("input-password"));
+        password.sendKeys("parola123!");
+        String password1= password.getText();
+        String actualPassword=password1;
+        String expectedPassword=password.getText();
+        Assert.assertEquals(actualPassword,expectedPassword);
+
+        if (password1.equalsIgnoreCase(expectedPassword)){
+            System.out.println("Successfully log in");
+
+        }
+        else  {
+            System.out.println("Wrong password");
+        }
+
+        WebElement logInButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        logInButton.click();
+
+        WebElement logOutButton = driver.findElement(By.xpath("//a[contains(@href, 'logout')]"));
+        String logOutButtonText = logOutButton.getText();
+        Assert.assertEquals(logOutButtonText, "Logout");
+
+        logOutButton.click();
+
+    }
+
+    @Test
+    public void dropDownOrderStatus(){
+
+        WebElement orderStatus =driver.findElement(By.name("filter_order_status_id"));
+        orderStatus.click();
+
+    }
+
+    @AfterMethod
+    public void afterTest(){
+
+        driver.quit();
+    }
+}
+
+
+
